77b01625562030cc40c606aa36fde7c17e0a10f1
Mona Miga <74436129+mona-miga@users.noreply.github.com>
2020-12-10 14:54:54 -0800

    add spec_helper and transpose rspec

--- W4D4/TDD Project/lib/tdd.rb
+++ W4D4/TDD Project/lib/tdd.rb
@@ -7,3 +7,16 @@ def my_unique(arr)
   end
   new_arr
 end
+
+def two_sum(arr)
+  new_arr = []
+  arr.each_with_index do |el1, idx1|
+    arr.each_with_index do |el2, idx2|
+      if idx1 < idx2 && el1 + el2 == 0
+        new_arr << [idx1, idx2]
+      end
+    end
+  end
+  new_arr
+end
+
