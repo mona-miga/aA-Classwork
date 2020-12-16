b4d4dc124e7c3bf565e62ce76e0be6a1e63a1d20
Mona Miga <monaleez@love.com>
2020-12-15 16:46:27 -0800

    worked on 07

--- W5D2/sql_zoo/lib/07_joins_exercises.rb
+++ W5D2/sql_zoo/lib/07_joins_exercises.rb
@@ -40,6 +40,14 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT
+    title
+  FROM
+    movies
+  JOIN castings on castings.movie_id = movies.id 
+  JOIN actors on castings.actor_id = actors.id
+  WHERE
+    name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +56,29 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+  SELECT
+    title
+  FROM
+    movies
+  JOIN castings on castings.movie_id = movies.id 
+  JOIN actors on castings.actor_id = actors.id
+  WHERE
+    name = 'Harrison Ford' AND ord != 1
+
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+  SELECT
+    title, name
+  FROM
+    movies
+  JOIN castings on castings.movie_id = movies.id 
+  JOIN actors on castings.actor_id = actors.id
+  WHERE
+    yr = 1962 and ord = 1
   SQL
 end
 
@@ -61,6 +86,18 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+  SELECT
+    yr, count(*)
+  FROM
+    movies
+  JOIN castings on castings.movie_id = movies.id 
+  JOIN actors on castings.actor_id = actors.id
+  WHERE
+    name = 'John Travolta'
+  GROUP BY
+    yr
+  HAVING 
+    count(*) >= 2
   SQL
 end
 
