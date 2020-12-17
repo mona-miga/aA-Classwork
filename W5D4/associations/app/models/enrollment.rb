582614967292741bb2f86f20bce9eb5f50c68989
Mona Miga <monaleez@love.com>
2020-12-17 14:48:37 -0800

    worked on models files

--- W5D4/associations/app/models/enrollment.rb
+++ W5D4/associations/app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ApplicationRecord
+  belongs_to :courses,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: 'Course'
+
+  belongs_to :students,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: 'User'
 end
