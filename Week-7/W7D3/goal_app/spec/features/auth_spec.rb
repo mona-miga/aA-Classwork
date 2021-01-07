2364394f3e838605eb2a2695f4ff2de528d9986a
Andrew Curd <andrewc2695@gmail.com>
2021-01-06 16:40:52 -0800

    wrote auth specs

--- Week-7/W7D3/goal_app/spec/features/auth_spec.rb
+++ Week-7/W7D3/goal_app/spec/features/auth_spec.rb
@@ -20,6 +20,47 @@ feature 'the signup process', type: :feature do
         user = User.find_by(username: 'capy')
         expect(current_url).to eq(user_url(user))
     end
+end
 
-    feature 
+feature 'logging in' do
+  it 'has a sign in page' do
+    visit new_session_path
+    expect(page).to have_content 'Sign In'
+  end
+
+  it 'takes a username and password' do
+    visit new_session_path
+    expect(page).to have_content 'Username'
+    expect(page).to have_content 'Password'
+  end
+
+  context 'on a successful save' do
+    it 'logs in a user and takes them to users index on success' do
+      User.create!(username: 'jack_bruce', password: 'abcdef')
+
+      # Sign in as newly created user
+      sign_in_as_jack_bruce
+      expect(current_path).to eq(users_path)
+    end
+  end
+  
+  context 'on a failed save' do
+    before :each do
+      visit new_session_path
+      fill_in 'Username', with: 'LillyBilly'
+      click_button 'Sign In'
+    end
+
+    it 'renders errors to the user' do
+      expect(page).to have_content "Invalid username or password"
+    end
+  end
+end
+
+
+feature 'logging out' do
+  scenario 'begins with a logged out state'
+
+  scenario 'doesn\'t show username on the homepage after logout'
+
 end
