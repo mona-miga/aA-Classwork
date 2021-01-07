c4eb9c9989d4223aea5fcabe7bf5f59f05100eca
Mona Miga <monaleez@love.com>
2021-01-06 16:21:56 -0800

    add auth_spec and html views

--- Week-7/W7D3/goal_app/spec/features/auth_spec.rb
+++ Week-7/W7D3/goal_app/spec/features/auth_spec.rb
@@ -13,9 +13,6 @@ feature 'the signup process', type: :feature do
 
     scenario 'redirects user to the users show page and displays username on success' do
         fill_in 'Username', with: 'capy'
-        fill_in 'Age', with: '6'
-        fill_in 'Political affiliation', with: 'hufflepuff'
-        fill_in 'Email', with: 'capy@aa.io'
         fill_in 'Password', with: 'password'
         click_button 'Create user'
         # save_and_open_page
@@ -23,4 +20,6 @@ feature 'the signup process', type: :feature do
         user = User.find_by(username: 'capy')
         expect(current_url).to eq(user_url(user))
     end
+
+    feature 
 end
